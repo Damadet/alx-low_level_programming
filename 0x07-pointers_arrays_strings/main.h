3baa54e5408f6c64992e998c7c532a1c55357c89
Adedokun Oluwadamilare <adedokundamilare41@gmail.com>
2022-04-26 14:02:11 -0700

    hi

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
